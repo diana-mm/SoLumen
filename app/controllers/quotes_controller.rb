d421861e150cd666a855a797824213a23bf2bf5d
diana-mm <dmmiller85@gmail.com>
2019-11-05 14:22:08 -0700

    controler

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,2 +1,12 @@
 class QuotesController < ApplicationController
+    def index
+        @quotes = Quote.all
+        render json: quote
+    end
+
+    def show
+        quote = Quote.find_by(id: params[:id])
+        render json: quote
+      end
+
 end
